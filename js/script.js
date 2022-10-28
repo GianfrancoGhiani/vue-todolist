c1f792e56c77510c49fee033353a99076a12e3a1
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-10-28 16:53:23 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -15,12 +15,15 @@ Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo vie
      rimosso dalla lista.
 
 MILESTONE 3
-Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+Predisporre un campo di input testuale e un pulsante "aggiungi": 
+cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un 
+nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 */
+
 const {createApp} = Vue;
 const app = Vue.createApp({
     data() {
@@ -40,9 +43,14 @@ const app = Vue.createApp({
                 {
                     text: 'pomodoro',
                     done: false
-                },
-                                        
-            ]
+                }                     
+            ],
+            newObj:{
+                text: '',
+                done: false
+            },
+            elemToAdd: '',
+            // cloneObj:'',
         }
     },
     methods: {
@@ -52,7 +60,27 @@ const app = Vue.createApp({
                 return 'text-decoration-line-through'
             }
         },
-        
+        //take the index and, at this splice, delete at this position 1 elem
+        deleteElement(index){
+            this.list.splice(index,1);
+            console.log(this.list)
+        },
+        // take the input text value and place it as the value of a "newObj.text"
+        addObjBot(toAdd){
+            let cloneObj = Object.assign({}, this.newObj);
+            cloneObj.text = toAdd;
+            this.elemToAdd='';
+            this.list.push(cloneObj);
+        },
+        // take the input text value and place it as the value of a "newObj.text"
+        addObjTop(toAdd){
+            let cloneObj = Object.assign({}, this.newObj);
+            cloneObj.text = toAdd;
+            this.elemToAdd='';
+            this.list.unshift(cloneObj);
+        }
     },
 })
 app.mount('#app')
+
+
